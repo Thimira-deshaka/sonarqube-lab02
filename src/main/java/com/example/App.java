c4b85ee3f7382f1c8a8278426002164e138bac0a
Thimira Deshaka <thimiradeshakashan0220@gmail.com>
2026-02-02 00:46:21 +0530

    Fix quality issues 2.0

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,7 +8,7 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         Calculator calc = new Calculator();
-        logger.info(calc.calculate(10, 5, "add-again"));
+        logger.info(String.valueOf(calc.calculate(10, 5, "add-again")));
         UserService service = new UserService();
         service.findUser("admin");
         service.deleteUser("admin"); // NEW dangerous call
